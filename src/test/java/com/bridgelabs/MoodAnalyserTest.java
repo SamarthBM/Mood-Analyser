ed4e69400e038f434658c40cd49682ad7ec7acf8
SamarthBM <samarthbm18@gmail.com>
2021-07-30 18:47:13 +0530

    Initial commit

---
+++ src/test/java/com/bridgelabs/MoodAnalyserTest.java
@@ -0,0 +1,13 @@
+//package com.bridgelabs;
+//
+//import org.junit.Assert;
+//import org.junit.Test;
+//
+//public class MoodAnalyserTest {
+//    @Test
+//    public void testMoodAnalysis() {
+//        MoodAnalyser moodAnalyser = new MoodAnalyser();
+//        String mood = moodAnalyser.analyseMood("This is a happy message");
+//        Assert.assertEquals(mood, "HAPPY");
+//    }
+//}
